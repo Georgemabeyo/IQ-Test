38eb09ff84b2ba6770ed075dac9173d0af4db351
Georgemabeyo <149334413+Georgemabeyo@users.noreply.github.com>
2025-08-09 18:04:05 +0300

    config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Angalia kama environment variable DATABASE_URL ipo (Render au hosting nyingine)
-$databaseUrl = getenv('DATABASE_URL');
+$databaseUrl = getenv('postgresql://iq_test_krgw_user:OgdTKE141PnrIgF8PPB1zhzAvK718bDi@dpg-d2bm45h5pdvs73ctek4g-a.oregon-postgres.render.com/iq_test_krgw');
 
 if ($databaseUrl) {
     // Pata vipengele vya muunganisho kutoka kwenye DATABASE_URL
@@ -12,10 +12,10 @@ if ($databaseUrl) {
     $database = ltrim($dbopts["path"], '/');
 } else {
     // Kama uko local XAMPP, tumia settings hizi za kawaida
-    $host = '127.0.0.1';      // Local server
-    $user = 'root';           // Default XAMPP user
-    $password = '';           // Default XAMPP password (hakuna password)
-    $database = 'iq_test';    // Hakikisha umeunda database hii ndani ya phpMyAdmin
+    $host = 'dpg-d2bm45h5pdvs73ctek4g-a';      // Local server
+    $user = 'iq_test_krgw_user';           // Default XAMPP user
+    $password = 'OgdTKE141PnrIgF8PPB1zhzAvK718bDi';           // Default XAMPP password (hakuna password)
+    $database = 'iq_test_krgw';    // Hakikisha umeunda database hii ndani ya phpMyAdmin
 }
 
 // Unda connection
@@ -26,3 +26,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
